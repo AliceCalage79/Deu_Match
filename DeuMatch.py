53e299ff544891f9265e00e626cc4b8fe80d0d49
Alice Calage Campos <calagecamposalice@gmail.com>
2023-05-15 19:02:00 -0300

    Colocando mais algumas funcionalidades

--- DeuMatch.py
+++ DeuMatch.py
@@ -8,23 +8,39 @@ def processoSeletivo(opcao): #def para licalizar qual o processo seletivo
         return ('Teste Prático')
     elif opcao == 4:
         return  ('Avaliação Soft Skill')
+    
+def buscaNota (palavra):
+    nota=[]
+    for i in range(len(palavra)):
+        if palavra [i].isdigit():
+            if palavra[i] != '0' and palavra [i-1] !='1' and palavra[i] != '1':
+                nota.append(int(palavra[i]))
+
+            elif palavra[i] == '1' and palavra[i+1]=='0':
+                nota.append (int(10))
+    return nota
+
 def buscaCandidato (notaMinima, listaPrincipal): 
- candidato=''
- contador = 0
- for x in range (0,len(listaPrincipal),1):
-     palavra = listaPrincipal[x][1]
-     if int(palavra[1]) >= notaMinima[0]:
-         if int(palavra[4])>= notaMinima[1]:
-             if int(palavra[7])>= notaMinima[2]:
-                 if int(palavra[10])>= notaMinima[3]:
-                     candidato =candidato + ' ' +listaPrincipal[x][0]+ listaPrincipal[x][1]
-                     contador = contador + 1
- if contador == 0:
-    print ('não ha candidato')
- else:
-    print ('======================================================')
-    print ('Os selecionados são:')
-    print (candidato)
+    candidato=''
+    contador = 0
+    for x in range (0,len(listaPrincipal),1):
+       palavra = listaPrincipal[x][1]
+       nota =buscaNota(palavra)
+       if int(nota[0]) >= notaMinima[0]:
+         
+            if int(nota[1])>= notaMinima[1]:
+             
+                if int(nota[2])>= notaMinima[2]:
+                    
+                    if int(nota[3])>= notaMinima[3]:
+                        candidato =candidato +str(x+1)+ '.' +listaPrincipal[x][0]+'       '+listaPrincipal[x][1]+' \n____________________________________\n'
+                        contador = contador + 1
+    if contador == 0:
+        print ('não ha candidato')
+    else:
+        print ('==============================')
+        print ('Os selecionados são:\n')
+        print (candidato)
 
 
 
@@ -32,28 +48,45 @@ listaPrincipal = []
 listaCandidatos =[]
 numProcessos = 5
 notaMinima =[]
+inicio =True
+while inicio:
+    print ('===================================================')
+    print ('\nÓla.''\nQuantos candidatos gostaria de cadastrar:\n')
+    numeroDeCandidatos =int(input())
 
-print ('Quantos candidatos gostaria de cadastrar:')
-numeroDeCandidatos =int(input())
+    for x in range  (0,numeroDeCandidatos,1):
+        guardaNotas = ('eX_tX_pX_sX_')
+        print ('Nome do candidato:\n')
+        nomeCandidato = input()
+        listaCandidatos.append(nomeCandidato)
 
-for x in range  (0,numeroDeCandidatos,1):
-    guardaNotas = ('eX_tX_pX_sX_')
-    print ('Nome do candidato:')
-    nomeCandidato = input()
-    listaCandidatos.append(nomeCandidato)
-    for i in range (1,numProcessos,1):
-        print ('Nota de '+(processoSeletivo(i))+'')
-        nota = input()
-        guardaNotas =  guardaNotas.replace('X',nota,1)
-    listaCandidatos.append(guardaNotas)
-    listaPrincipal.append(listaCandidatos)
-    listaCandidatos=[]
+        for i in range (1,numProcessos,1):
+            print ('\nNota de '+(processoSeletivo(i))+':')
+            nota = input()
+            guardaNotas =  guardaNotas.replace('X',nota,1)
+        listaCandidatos.append(guardaNotas)
+        listaPrincipal.append(listaCandidatos)
+        listaCandidatos=[]
+    cadastro =True
+    while cadastro:
+        print ('============================================')
+        print( 'Por favor digite a nota mínima nos processos,''\npara que retorne os candidatos selecionados:\n')
 
+        for i in range (1,numProcessos,1):
+            print ('\nNota de '+(processoSeletivo(i))+':')
+            nota= int(input())
+            notaMinima.append(nota)
+        buscaCandidato (notaMinima, listaPrincipal)
 
-
-print( 'Por favor digite a nota mínima nos processos, para que retorne os candidatos selecionados:')
-for i in range (1,numProcessos,1):
-      print ('Nota de '+(processoSeletivo(i))+'')
-      nota= int(input())
-      notaMinima.append(nota)
-buscaCandidato (notaMinima, listaPrincipal)
+        print ('Gostaria de voltar a consultar candidatos ou cadastrar novos: \n1.Consultar\n2.Cadastrar\n3.Sair')
+        opcaofinal = input()
+        while  not opcaofinal in ('1','2','3'):
+            print('\n Opção Invalida digite novamente:  \n1.Consultar\n2.Cadastrar\n3.Sair')
+            opcaofinal =input()
+        if opcaofinal == '1':
+            cadastro = True
+        elif opcaofinal =='2':
+            cadastro =False
+            inicio == True
+        elif opcaofinal == '3':
+            exit()
